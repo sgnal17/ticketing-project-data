49bacce50a7b758677ff667a940f52b6f94fdede
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-04-28 22:51:30 -0400

    connection string added andcreated table through entity classes

---
+++ src/main/java/com/cydeo/MyTicketingProjectDataApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTicketingProjectDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTicketingProjectDataApplication.class, args);
+    }
+
+}
--- src/main/java/com/cydeo/entity/Role.java
+++ src/main/java/com/cydeo/entity/Role.java
@@ -4,12 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Role {
+@Entity
+@Table(name = "roles")
+public class Role extends BaseEntity{
 
-    private Long id;
+
     private String description;
 
 }
