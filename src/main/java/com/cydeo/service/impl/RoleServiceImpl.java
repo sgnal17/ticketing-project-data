9391d5f7803353fe4ac13be2919214e9921e56df
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-04-29 20:10:40 -0400

    method impl added

--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -33,6 +33,7 @@ public class RoleServiceImpl implements RoleService {
     @Override
     public RoleDTO findById(Long id) {
 
-        return null;
+        return roleMapper.convertToDto( roleRepository.findById(id).get());
+
     }
 }
