c1e6c263c0033025eab72bbda98f88bbbd2c2ec0
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-04-30 11:23:23 -0400

    fake user added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,9 @@ INSERT INTO roles(description)
 VALUES ('Admin'),
        ('Manager'),
        ('Employee');
+
+
+insert into users(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id, enabled,
+                  first_name, gender, last_name, user_name, role_id)
+values ('2022-01-05 00:00:00', 1, false, '2022-01-05 00:00:00', 1, true, 'admin', 'MALE', 'admin', 'admin@admin.com',
+        1);
