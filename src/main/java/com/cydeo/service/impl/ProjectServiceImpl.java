9be16a7af7e727194caf4413dfe42a3d404651f4
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-05-03 13:17:16 -0400

    methods and implementation added ,to see project status

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -1,11 +1,15 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
 import com.cydeo.entity.Project;
+import com.cydeo.entity.User;
 import com.cydeo.enums.Status;
 import com.cydeo.mapper.ProjectMapper;
+import com.cydeo.mapper.UserMapper;
 import com.cydeo.repository.ProjectRepository;
 import com.cydeo.service.ProjectService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,10 +20,14 @@ public class ProjectServiceImpl implements ProjectService {
 
     private final ProjectRepository projectRepository;
     private final ProjectMapper projectMapper;
+    private final UserService userService;
+    private final UserMapper userMapper;
 
-    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectMapper projectMapper) {
+    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectMapper projectMapper, UserService userService, UserMapper userMapper) {
         this.projectRepository = projectRepository;
         this.projectMapper = projectMapper;
+        this.userService = userService;
+        this.userMapper = userMapper;
     }
 
     @Override
@@ -69,5 +77,23 @@ public class ProjectServiceImpl implements ProjectService {
       Project project=projectRepository.findByProjectCode(code);
       project.setProjectStatus(Status.COMPLETE);
       projectRepository.save(project);
+    }
+
+    @Override
+    public List<ProjectDTO> listAllProjectDetails() {
+      UserDTO currentUserDTO= userService.findByUserName("harold@manager.com");
+      User user=userMapper.convertToEntity(currentUserDTO);
+
+      List<Project> projectList=projectRepository.getProjectsByAssignedManager(user);
+    return  projectList.stream().map(project -> {
+         ProjectDTO obj= projectMapper.convertToDto(project);
+
+
+
+         return obj;
+      }).collect(Collectors.toList());
+
+
+
     }
 }
